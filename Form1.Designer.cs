a8a1505662cd49ef2ae2206594c81f13c918504c
MahidDandamun <Dandamunmahid@gmail.com>
2022-07-10 01:19:59 +0800

    Initial Commit, Design is done

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Resume_Creator
+{
+    partial class FrmResumeCreator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmResumeCreator));
+            this.pnl1 = new System.Windows.Forms.Panel();
+            this.pbxLogo = new System.Windows.Forms.PictureBox();
+            this.lblDesign = new System.Windows.Forms.Label();
+            this.cbxNavigateFiles = new System.Windows.Forms.ComboBox();
+            this.btnCreateResume = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.pbxClose = new System.Windows.Forms.PictureBox();
+            this.pnl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl1
+            // 
+            this.pnl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.pnl1.Controls.Add(this.pbxLogo);
+            this.pnl1.Controls.Add(this.lblDesign);
+            this.pnl1.Location = new System.Drawing.Point(-2, -1);
+            this.pnl1.Name = "pnl1";
+            this.pnl1.Size = new System.Drawing.Size(303, 463);
+            this.pnl1.TabIndex = 0;
+            // 
+            // pbxLogo
+            // 
+            this.pbxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbxLogo.Image")));
+            this.pbxLogo.Location = new System.Drawing.Point(14, 78);
+            this.pbxLogo.Name = "pbxLogo";
+            this.pbxLogo.Size = new System.Drawing.Size(262, 182);
+            this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxLogo.TabIndex = 2;
+            this.pbxLogo.TabStop = false;
+            // 
+            // lblDesign
+            // 
+            this.lblDesign.AutoSize = true;
+            this.lblDesign.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDesign.ForeColor = System.Drawing.Color.White;
+            this.lblDesign.Location = new System.Drawing.Point(14, 328);
+            this.lblDesign.Name = "lblDesign";
+            this.lblDesign.Size = new System.Drawing.Size(162, 126);
+            this.lblDesign.TabIndex = 1;
+            this.lblDesign.Text = "Your \r\nResume \r\nCreator";
+            // 
+            // cbxNavigateFiles
+            // 
+            this.cbxNavigateFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbxNavigateFiles.FormattingEnabled = true;
+            this.cbxNavigateFiles.Location = new System.Drawing.Point(389, 213);
+            this.cbxNavigateFiles.Name = "cbxNavigateFiles";
+            this.cbxNavigateFiles.Size = new System.Drawing.Size(350, 23);
+            this.cbxNavigateFiles.TabIndex = 1;
+            // 
+            // btnCreateResume
+            // 
+            this.btnCreateResume.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.btnCreateResume.FlatAppearance.BorderSize = 0;
+            this.btnCreateResume.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreateResume.Font = new System.Drawing.Font("Century Schoolbook", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCreateResume.ForeColor = System.Drawing.Color.White;
+            this.btnCreateResume.Location = new System.Drawing.Point(472, 327);
+            this.btnCreateResume.Name = "btnCreateResume";
+            this.btnCreateResume.Size = new System.Drawing.Size(145, 36);
+            this.btnCreateResume.TabIndex = 2;
+            this.btnCreateResume.Text = "Create Resume";
+            this.btnCreateResume.UseVisualStyleBackColor = false;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblFileName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.lblFileName.Location = new System.Drawing.Point(307, 215);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(76, 17);
+            this.lblFileName.TabIndex = 3;
+            this.lblFileName.Text = "File Name:";
+            // 
+            // pbxClose
+            // 
+            this.pbxClose.Image = ((System.Drawing.Image)(resources.GetObject("pbxClose.Image")));
+            this.pbxClose.Location = new System.Drawing.Point(730, 6);
+            this.pbxClose.Name = "pbxClose";
+            this.pbxClose.Size = new System.Drawing.Size(40, 30);
+            this.pbxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxClose.TabIndex = 4;
+            this.pbxClose.TabStop = false;
+            this.pbxClose.Click += new System.EventHandler(this.pbxClose_Click);
+            // 
+            // FrmResumeCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.ClientSize = new System.Drawing.Size(772, 459);
+            this.Controls.Add(this.pbxClose);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.btnCreateResume);
+            this.Controls.Add(this.cbxNavigateFiles);
+            this.Controls.Add(this.pnl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmResumeCreator";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Resume Creator";
+            this.pnl1.ResumeLayout(false);
+            this.pnl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Label lblDesign;
+        private System.Windows.Forms.PictureBox pbxLogo;
+        private System.Windows.Forms.ComboBox cbxNavigateFiles;
+        private System.Windows.Forms.Button btnCreateResume;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.PictureBox pbxClose;
+    }
+}
+
